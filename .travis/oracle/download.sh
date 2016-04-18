1ff6de2f6e2d9852d0b96ba74468d0ed1fefbd99
Rapševičius Valdas <vrapsevicius@gmail.com>
2016-04-18 14:06:48 +0300

    Fixed travis build

--- .travis/oracle/download.sh
+++ .travis/oracle/download.sh
@@ -1,16 +1,10 @@
 #!/bin/sh -e
 
-[ -n "$ORACLE_COOKIE" ] || { echo "Missing ORACLE_COOKIE environment variable!"; exit 1; }
 [ -n "$ORACLE_FILE" ] || { echo "Missing ORACLE_FILE environment variable!"; exit 1; }
+[ -n "$ORACLE_URL" ] || { echo "Missing ORACLE_URL environment variable!"; exit 1; }
+[ -n "$ORACLE_URL_USER" ] || { echo "Missing ORACLE_URL_USER environment variable!"; exit 1; }
+[ -n "$ORACLE_URL_PASS" ] || { echo "Missing ORACLE_URL_PASS environment variable!"; exit 1; }
 
 cd "$(dirname "$(readlink -f "$0")")"
 
-npm install bluebird node-phantom-simple
-
-export COOKIES='cookies.txt'
-export USER_AGENT='Mozilla/5.0'
-
-echo > "$COOKIES"
-chmod 600 "$COOKIES"
-
-exec node download.js
+wget --user="$ORACLE_URL_USER" --password="$ORACLE_URL_PASS" "$ORACLE_URL"
