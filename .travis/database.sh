d0affbaa7c6c28bfe41a0b499f69aab06049ff00
Rapševičius Valdas <vrapsevicius@gmail.com>
2016-05-23 12:48:23 +0300

    Fixed travis tests

--- .travis/database.sh
+++ .travis/database.sh
@@ -4,4 +4,4 @@ cd src/main/ddl
 "$ORACLE_HOME/bin/sqlplus" -L / AS SYSDBA @create.sql
 
 cd ../../test/ddl
-"$ORACLE_HOME/bin/sqlplus" -L / AS SYSDBA @create.sql
+"$ORACLE_HOME/bin/sqlplus" -L / AS SYSDBA @create.sql xe
