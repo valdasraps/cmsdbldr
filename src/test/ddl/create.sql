70d06778df0db1e5830444e529c18794f1fef0e8
jmonkevicius <monkelis0@gmail.com>
2016-05-23 12:08:47 +0300

    Created clean srcipt for testing, PropertiesTest updated for checking values from source, added test orderin (TestSuite.java)

--- src/test/ddl/create.sql
+++ src/test/ddl/create.sql
@@ -2,27 +2,27 @@ WHENEVER SQLERROR EXIT SQL.SQLCODE
 
 
 PROMPT Executing CMS_TST_CORE_ATTRIBUTE stuff
-connect CMS_TST_CORE_ATTRIBUTE/testing
+connect CMS_TST_CORE_ATTRIBUTE/testing@&&1
 
 @01-core_attribute
 
 PROMPT Executing CMS_TST_CORE_CONSTRUCT stuff
-connect CMS_TST_CORE_CONSTRUCT/testing
+connect CMS_TST_CORE_CONSTRUCT/testing@&&1
 
 @02-core_construct
 
 PROMPT Executing CMS_TST_CORE_COND stuff
-connect CMS_TST_CORE_COND/testing
+connect CMS_TST_CORE_COND/testing@&&1
 
 GRANT SELECT ON CMS_TST_CORE_COND.ANY_COND_RECORD_ID_SEQ TO CMS_TST_TEST_COND;
 
 PROMPT Executing CMS_TST_TEST_COND stuff
-connect CMS_TST_TEST_COND/testing
+connect CMS_TST_TEST_COND/testing@&&1
 
 @03-test_cond
 
 PROMPT Executing CMS_TST_CORE_COND stuff
-connect CMS_TST_CORE_COND/testing
+connect CMS_TST_CORE_COND/testing@&&1
 
 @04-core_cond
 
---
+++ src/test/java/org/cern/cms/dbloader/tests/TestSuite.java
@@ -0,0 +1,21 @@
+package org.cern.cms.dbloader.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	PartLoadTest.class,
+	CondInfoTest.class, 
+	ConditionXmlTest.class, 
+	CondLoadTest.class,
+	CondLoadZipTest.class,
+	LobManagerTest.class, 
+	PropertiesTest.class, 
+	PropertyTypeTest.class, 
+	TablePrinterTest.class 
+	})
+public class TestSuite {
+
+}
