bdb5154aee5ae77fa6e825214d42226913a52669
apoluden <artiomnkkm@gmail.com>
2018-10-24 14:34:55 +0300

    service append: load files

--- src/main/java/org/cern/cms/dbloader/rest/provider/Load.java
+++ src/main/java/org/cern/cms/dbloader/rest/provider/Load.java
@@ -1,19 +1,26 @@
 package org.cern.cms.dbloader.rest.provider;
 
 import com.google.inject.Injector;
+
+import java.io.*;
 import java.nio.file.Files;
 import java.util.Collections;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.Response;
 import lombok.extern.log4j.Log4j;
+import org.apache.commons.io.FilenameUtils;
 import org.cern.cms.dbloader.DbLoader;
 import org.cern.cms.dbloader.manager.FilesManager;
 import org.cern.cms.dbloader.manager.PropertiesManager;
 import org.cern.cms.dbloader.manager.file.FileBase;
 import org.cern.cms.dbloader.rest.Application;
+
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 
 /**
  * Data loading entry point for RESTful API
@@ -43,13 +50,46 @@ public class Load extends ProviderBase {
     public final Response loadXml(String data) {
         return load(data, "xml");
     }
-    
-    private final Response load(final String data, final String ext) {
+
+    @POST
+    @Path("/load/file")
+    @Consumes({MediaType.MULTIPART_FORM_DATA})
+    public final Response loadZip(@FormDataParam("uploadFile") InputStream fileInputStream,
+                                  @FormDataParam("uploadFile") FormDataContentDisposition fileFormDataContentDisposition) {
+        String ext = FilenameUtils.getExtension(fileFormDataContentDisposition.getFileName());
+        return load(fileInputStream, ext);
+    }
+
+    private String writeToFileServer(InputStream inputStream, String path) throws IOException {
+
+        OutputStream outputStream = null;
+
+        try {
+            outputStream = new FileOutputStream(new File(path));
+            int read = 0;
+            byte[] bytes = new byte[1024];
+            while ((read = inputStream.read(bytes)) != -1) {
+                outputStream.write(bytes, 0, read);
+            }
+            outputStream.flush();
+        }
+        catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        finally{
+            //release resource, if any
+            outputStream.close();
+        }
+        return path;
+    }
+
+
+    private final Response load(final InputStream inputStream, final String ext) {
         try {
 
             final java.nio.file.Path file = Files.createTempFile("Load.", ".".concat(ext));
-            Files.write(file, data.getBytes());
-            
+            writeToFileServer(inputStream, file.toAbsolutePath().toString());
+
             log.info(String.format("Load request: %s", file.toAbsolutePath()));
             
             DbLoader loader = new DbLoader(pm);
@@ -66,5 +106,27 @@ public class Load extends ProviderBase {
             
         }
     }
-    
+
+    private final Response load(final String data, final String ext) {
+        try {
+
+            final java.nio.file.Path file = Files.createTempFile("Load.", ".".concat(ext));
+            Files.write(file, data.getBytes());
+
+            log.info(String.format("Load request: %s", file.toAbsolutePath()));
+
+            DbLoader loader = new DbLoader(pm);
+            for (FileBase fb: fm.getFiles(Collections.singletonList(file.toAbsolutePath().toString()))) {
+                loader.loadArchive(injector, fb);
+            }
+
+            return Response.status(Response.Status.OK).entity("loaded").type(MediaType.MULTIPART_FORM_DATA).build();
+
+        } catch (Throwable ex) {
+
+            log.warn(ex.getMessage());
+            throw buildException(ex);
+
+        }
+    }
 }
