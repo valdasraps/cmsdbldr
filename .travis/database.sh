de05e2bb4ff9926e3488621a64d384ba98d2d3b3
Rapševičius Valdas <vrapsevicius@gmail.com>
2019-01-21 13:35:22 +0200

    Added tracking DDLs to travis

--- .travis/database.sh
+++ .travis/database.sh
@@ -2,6 +2,7 @@
 
 cd src/main/ddl
 "$ORACLE_HOME/bin/sqlplus" -L / AS SYSDBA @create.sql xe
+"$ORACLE_HOME/bin/sqlplus" -L / AS SYSDBA @create_tracking.sql xe
 
 cd ../../test/ddl
 "$ORACLE_HOME/bin/sqlplus" -L / AS SYSDBA @create.sql xe
