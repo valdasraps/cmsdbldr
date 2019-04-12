f5c53b7c41243b3b6d62fa330c146dd6c54502e2
Aivaras Šilalė <Aivaras.silale@gmail.com>
2019-04-12 15:00:27 +0300

    Added PART_DETAILS to Travis config

--- .travis/database.sh
+++ .travis/database.sh
@@ -3,6 +3,7 @@
 cd src/main/ddl
 "$ORACLE_HOME/bin/sqlplus" -L / AS SYSDBA @create.sql xe
 "$ORACLE_HOME/bin/sqlplus" -L / AS SYSDBA @create_tracking.sql xe
+"$ORACLE_HOME/bin/sqlplus" -L / AS SYSDBA @create_part_details.sql xe
 
 cd ../../test/ddl
 "$ORACLE_HOME/bin/sqlplus" -L / AS SYSDBA @create.sql xe
