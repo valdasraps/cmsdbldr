cfb76233b122cb0615c7c45fd518119ba90d4684
Rapševičius Valdas <vrapsevicius@gmail.com>
2019-10-29 15:04:52 +0200

    Updated python client to support SSO

--- src/main/python/cmsdbldr_client.py
+++ src/main/python/cmsdbldr_client.py
@@ -2,30 +2,30 @@ from optparse import OptionParser
 import requests
 import sys
 import os
+import re
+import importlib
 from requests.auth import HTTPBasicAuth
 import getpass
 
-USAGE = 'usage: %prog [-h HOST] [-p PORT] [-u USER] -k KEY -d DET -b DAT FILE [FILE]'
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-DEFAULT_HOST = "localhost"
-DEFAULT_PORT = 80
+USAGE = 'usage: %prog --url=URL FILE'
+
 DEFAULT_USER = getpass.getuser()
-DEFAULT_KEY = 'somekey'
+SSO_COOKIE_PROVIDER = "cern_sso_api:cern_sso_cookies"
+SSO_LOGIN_URL = "https://login.cern.ch/"
 
 ALLOWED_EXTENSIONS = ['xml','zip']
 
-class CLIClient:
+class LoaderClient:
     
     def __init__(self):
         
         self.parser = OptionParser(USAGE)
-        self.parser.add_option("-H", "--host",     dest = "host",     help = "service HOST (default: %s)" % DEFAULT_HOST, metavar = "HOST", default = DEFAULT_HOST)
-        self.parser.add_option("-p", "--port",     dest = "port",     help = "service PORT (default: %s)" % DEFAULT_PORT, metavar = "PORT", default = DEFAULT_PORT, type="int")
-        self.parser.add_option("-u", "--user",     dest = "user",     help = "uploading USER (default: %s)" % DEFAULT_USER, metavar = "USER", default = DEFAULT_USER)
-        self.parser.add_option("-k", "--key",      dest = "key",      help = "service KEY", metavar = "KEY")
-        self.parser.add_option("-d", "--det",      dest = "det",      help = "DETECTOR indicator", metavar = "DET")
-        self.parser.add_option("-b", "--dat",      dest = "dat",      help = "DATABASE indicator", metavar = "DAT")
-        self.parser.add_option("-q", "--quiet",    dest = "quiet",    help = "Do not print error, just return its code. OK = 0", action = "store_true", default = False)
+        self.parser.add_option("-u", "--url",   dest = "url",   help = "service URL", metavar = "url")
+        self.parser.add_option("-o", "--sso",   dest = "sso",   help = "use cookie provider from cern_sso_api module", metavar = "sso", action = "store_true", default=False)
+        self.parser.add_option("-q", "--quiet", dest = "quiet", help = "Do not print error, just return its code. OK = 0", action = "store_true", default = False)
 
     def _allowed_file(self, filename):
         return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
@@ -36,46 +36,70 @@ class CLIClient:
 
             (options, args) = self.parser.parse_args()
 
-            if options.key is None:
-                self.parser.error('Please provide service key')
+            if options.url is None:
+                self.parser.error('Please provide service URL')
 
-            if options.det is None:
-                self.parser.error('Please provide detector indicator')
+            url = options.url
+            while url.endswith('/'): url = url[:-1]
+            if not re.search("/load/file$", url):
+                url = url + "/load/file"
 
-            if options.dat is None:
-                self.parser.error('Please provide destination database indicator')
-
-            if len(args) == 0:
-                self.parser.error('Please provide at least one data file?')
+            if len(args) != 1:
+                self.parser.error('Please provide one data file?')
                 return 1
-            
-            files = []
-            for f in args:
-                if not (os.path.isfile(f) and os.path.exists(f)):
-                    self.parser.error('File [%s] not found or is not a file?' % f)
-                    return 2
-                if not self._allowed_file(f):
-                    self.parser.error('File [%s] allowed? Allowed extensions are (%s)' % (f, ','.join(ALLOWED_EXTENSIONS)))
-                    return 2
-                files.append(f)
 
-            url='/'.join(('http:/', options.host + ':' + str(options.port), 'cmsdbldr', options.det, options.dat))
-            for f in files:
-                files = {'file': open(f, 'rb')}
-                r = requests.post(url, files=files, auth=HTTPBasicAuth(options.user, options.key))
+            cprov = None
+            if options.sso and re.search("^https", url):
+                mod_name, fun_name = SSO_COOKIE_PROVIDER.split(":")
+                m = importlib.import_module(mod_name)
+                cprov = getattr(m, fun_name)
+            
+            f = args[0]
+            if not (os.path.isfile(f) and os.path.exists(f)):
+                self.parser.error('File [%s] not found or is not a file?' % f)
+                return 2
+            if not self._allowed_file(f):
+                self.parser.error('File [%s] allowed? Allowed extensions are (%s)' % (f, ','.join(ALLOWED_EXTENSIONS)))
+                return 2
+
+            files = {'uploadFile': open(f, 'rb')}
+            force_level = 0
+            while True:
+
+                cookies = None
+                if cprov is not None:
+                    cookies = cprov(url, force_level = force_level)
+
+                r = requests.post(url = url, files = files, cookies = cookies, verify = False)
+
+                if r.status_code == 200 and r.url.startswith(SSO_LOGIN_URL):
+                    if force_level < 2:
+                        force_level = force_level + 1
+                        continue
+                    else:
+                        if not options.sso:
+                            raise Exception('Resource is secured by SSO. Please try --sso')
+                        else:
+                            raise Exception('Error while logging to HTTPS/SSO')
+
                 if not options.quiet:
-                    print r.text
-                    print "Response code: %d" % r.status_code
+                    print(r.text)
+                    print("Response code: %d" % r.status_code)
+
                 if r.status_code == requests.codes.ok:
                     return 0
+
                 else:
                     return int(str(r.status_code)[0])
 
-        except Exception, e:
+        except Exception as e:
             
-            print "ERROR: %s\nDetails: %s" % (type(e).__name__, e)
+            import traceback
+            traceback.print_exc()
+
+            print("ERROR: %s\nDetails: %s" % (type(e).__name__, e))
 
 if __name__ == '__main__':
 
-    cli = CLIClient()
+    cli = LoaderClient()
     sys.exit(cli.run())
